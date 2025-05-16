177143c03f22be5f95f2595c496c34035817fcc8
Zac Healy <zac.healy@hotmail.co.uk>
2025-05-16 20:05:51 +0100

    Files inputted from program arguments

--- src/main.c
+++ src/main.c
@@ -6,14 +6,17 @@
 #include "../include/file_io.h"
 #include "../include/phone_record.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <input-file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    const char *filepath = argv[1];
+
     // Task 2 and 4
     printf("---------Task 2 and 4---------\n");
     phone_record_t *book = nullptr;
-    const unsigned long long count = load_phone_records(
-        "/Users/zachealy/development/repositories/telephone-record-management/P2-input-2.txt",
-        &book
-    );
+    const unsigned long long count = load_phone_records(filepath, &book);
 
     if (!book && count == 0) {
         fprintf(stderr, "Failed to load records\n");
@@ -22,14 +25,14 @@ int main(void) {
 
     printf("Loaded %llu records\n\n", count);
 
-    for (unsigned long long i = 0; i < count; ++i) {
-        printf("%hu %hu %llu  %s\n",
-               book[i].year_created,
-               book[i].country_code,
-               book[i].phone_number,
-               book[i].full_name
-        );
-    }
+    // for (unsigned long long i = 0; i < count; ++i) {
+    //     printf("%hu %hu %llu  %s\n",
+    //            book[i].year_created,
+    //            book[i].country_code,
+    //            book[i].phone_number,
+    //            book[i].full_name
+    //     );
+    // }
 
     // Task 3
     printf("\n");
